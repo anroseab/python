5cac56b1f72d5b0ed131f5c396cfff2774817593
anroseab <anroseab@gmail.com>
2023-09-06 03:04:51 -0700

    added

---
+++ main.py
@@ -0,0 +1,2 @@
+name="anrose"
+print("hello",name)
---
+++ operation.py
@@ -0,0 +1,10 @@
+a=10
+b=30
+print("a+b=",a+b)
+print("a-b=",a-b)
+print("a*b=",a*b)
+print("a/b=",a/b)
+print("a % b=",a%b)
+print("a**b=",a**b)
+print("a//b=",a//b)
+print("addition",a+b, "substraction",a-b, )
