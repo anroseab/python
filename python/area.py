8b02b080012f3f3d264204884942195742912b6c
anroseab <anroseab@gamil.com>
2023-09-12 03:20:31 -0700

    updated

---
+++ python/area.py
@@ -0,0 +1,7 @@
+l=int(input("enter l"))
+b=int(input("enter b"))
+a=(l*b)
+print('area=',a)
+p=(2*l*b)
+print('perimeter=',p)
+
---
+++ python/number.py
@@ -0,0 +1,13 @@
+
+a=int(input('enter 1st number'))
+b=int(input('enter 2nd number'))
+print("before")
+print(a)
+print(b)
+ 
+a,b=b,a
+
+
+print("after")
+print(a)
+print(b)
