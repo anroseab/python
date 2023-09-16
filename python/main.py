ce542f9db6b09f727cd80932df79e2f0391605f5
anroseab <anroseab@gmail.com>
2023-09-16 15:53:01 +0530

    added

--- python/main.py
+++ python/main.py
@@ -1,2 +1,22 @@
-name="anrose"
-print("hello",name)
+# birth_year=int(input("enter your birth year"))
+# if birth_year>=2000:
+#     print("20's kid")
+# else:
+#     print("90's kid")
+a=str(input("enter your password"))
+if len(a)>=8:
+       for i in a:
+              if i.isdigit():
+                     num=True
+              if i.isalnum():
+                     symbol=True
+       
+
+             
+
+    
+   
+     
+
+
+
---
+++ python/multiplicationtable.py
@@ -0,0 +1,6 @@
+# a=int(input('enter a number'))
+a=[5,4,3,6,7,3,2,1]
+for i in  a:
+
+    print(i*'*')
+# print('='*10)
