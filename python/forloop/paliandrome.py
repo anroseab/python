4edbde280dd0c819ca3cb7cccbee81817e057891
anroseab <anroseab@gmail.com>
2023-10-07 15:53:48 +0530

    updated

---
+++ python/forloop/paliandrome.py
@@ -0,0 +1,62 @@
+# x=input("enter a number")
+# # x=101
+# if x==x[::-1]:
+#     print("True")
+# else:
+#     print("False")
+
+
+# merge sort
+# list1=[1,3,5]
+# list2=[2,4,6]
+# list3=list1+list2
+# list3.sort()
+# print(list3)
+
+# duplicate
+# a=[1,2,6,6,8,8]
+# b=set(a)
+# c=list(b)
+# print(len(c))
+# c.sort()
+# print(c)
+
+
+
+
+# lenght of last word
+# a='qeqerw dakroai jnahusdf www'
+# b=a.split()
+# c=b[-1]
+# print(len(c))
+
+# 206 reverse
+# li=[1,2,3,4,8]
+# # b=li[-1]
+# print(li)
+# b=li[::-1]
+# print(b)
+
+
+# 217
+# a=[1,2,3,1]
+# b=set(a)
+# if a==b:
+#     print("false")
+# else:
+#     print("true")
+
+
+# 27 remove
+# num=[1,1,2,3,1]
+# value=1
+# for i in num:
+#     if i==value:
+#         num.remove(i)
+# print("lenght",len(num))
+# print(num)
+
+# 70
+# n=3
+# for i in n:
+#     if i==1:
