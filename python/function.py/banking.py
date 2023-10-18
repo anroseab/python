a8a2fdd2141398d4fd27821ebb1e78684fbbe7f6
anroseab <anroseab@gmail.com>
2023-10-18 15:54:06 +0530

    uploading

--- python/function.py/banking.py
+++ python/function.py/banking.py
@@ -2,9 +2,8 @@ bank={}
 account_no=1201
 li=[]
 acc=[]
-
 while True:
-    print("1:create account ","2:view accound ","3:add balance","4:withdraw balance","5:exit")
+    print("1:create account ","2:view account ","3:add balance","4:withdraw balance","5:exit")
     choice=int(input('enter your choice'))
     if choice==1:
         name=input("enter your name")
@@ -20,33 +19,37 @@ while True:
         account_no=account_no+1
         li.append(bank.copy())
     elif choice==2:
-          
-          print(li)
+          for ik in li:
+             print("-"*20)
+             for j,k in bank.items():
+              print(j,":",k)
+          for ik in range(1):
+             print("-"*20)
     elif choice==3:
         account=int(input('enter your account number'))
-        amount=int(input('enter your amount'))
         for i in li:
           acc.append(i['Acc.no'])
-        # print(acc)
         if account in acc:
-         for i in li:   
-            if i['Acc.no']==account:
-              i['balance']+=amount
+          amount=int(input('enter your amount'))
+          for i in li:   
+              if i['Acc.no']==account:
+                 i['balance']+=amount
         else:
                print("no account found")
        
     elif choice==4:
         account_no=int(input('enter your account number'))
-        with_draw=int(input('enter with draw amount'))
         for i in li:
-          acc.append(i['Acc.no'])
-        #   print(acc)
-        if account in acc:
-         for i in li:   
-            if i['Acc.no']==account:
-              if i['balance']>=with_draw:
-               i['balance']-=with_draw
+            acc.append(i['Acc.no'])
+        if account_no in acc:
+            with_draw=int(input('enter with draw amount'))
+            for i in li:   
+                if i['Acc.no']==account_no:
+                    if i['balance']>=with_draw:
+                        i['balance']-=with_draw
+                    else:
+                        print("not possible to withdraw")
         else:
-            print("not posible")
+                  print("no account found")
     elif choice==5:
         exit()
