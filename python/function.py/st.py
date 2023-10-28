e46115e966b6452be0eb5b182085e3b5c770247f
anroseab <anroseab@gmail.com>
2023-10-28 15:48:38 +0530

    uploading

--- python/function.py/st.py
+++ python/function.py/st.py
@@ -1,14 +1,14 @@
 student={}
-
 reg=100
 st=[]
 tea=[]
 store=[]
 teacher=[]
-stor=[]
+sto=[]
 teache=[]
+
 while True:
-    print("1:student","2:Teacher","3:view student","4:view teachers","5:assign teacher","6:class","7:exit")
+    print("1:student","2:Teacher","3.view","4:assign teacher","5:class","6:fees","7:teacher_st","8:fees_st","5:exit")
     choice=int(input("enter your choice"))
     if choice==1:
         name=input("enter ur name")
@@ -19,21 +19,19 @@ while True:
         student['class']=clas
         student["register_no"]=reg
         reg=reg+1
+        fe=12000
+        student['fees_balance']=fe
         st.append(student.copy())
 
     if choice==2:
         name=input("enter ur name")
         tea.append(name)
-        # clas=int(input("enter class"))
-        # tea.append(clas)
+        
     if choice==3:
         print(st)
+    # if choice==4:
+    #     print(tea)
     if choice==4:
-        print(tea)
-    if choice==5:
-        
-        
-
         stu=input("enter the name of student")
         for i in st:
           store.append(i["name"])
@@ -47,8 +45,12 @@ while True:
                     i['teacher']=tea[j]
          else:
               print("the student is not found")  
-        print(st)
-    if choice==6:
+        for i in st:           
+          print("-"*20)
+          for j,k in i.items():
+              print(j,":",k)
+          print("-"*20)
+    if choice==5:
 
        st_class=int(input("enter the class"))
        for i in st:
@@ -63,8 +65,49 @@ while True:
                      i['teacher']=tea[0]
                 else:
                       i["teacher"]=tea[1]
-       print(st) 
+       for i in st:           
+          print("-"*20)
+          for j,k in i.items():
+              print(j,":",k)
+          print("-"*20) 
                
+    if choice==6:
+        name=input("enter ur name")
+        clas=int(input("enter your clas"))
+        for i in st:
+          store.append(i["class"])
+        for i in st:
+          store.append(i["name"])  
+        if clas  in store:
+          if name in store: 
+           amount=int(input("enter the amount"))
+           for i in st:
+              if i["class"]==clas and i['name']==name:
+                    i["paid"]=amount
+                    i['fees_balance']=fe-amount
+                 
+                    if i['fees_balance']==0:
+                       i['fees_balance']=0
+           print(st)  
+          else:
+            print("student not fount")   
+        else:
+            print("student not fount")         
     if choice==7:
-        exit()
+       teacher=input("enter the teacher name")
+     
+       if teacher in tea:
+           for i in st:
+              if i['teacher']==teacher:
+                  teache.append(i['name'])
+           print(teache)
+    if choice==8:
+       for i in st:
+           
+          #  nam.append(i['name'])
+          #  paid.append(i['paid'])
+          #  fees.append(i['fees_balance'])
+           print("name:",i['name'], ',  paid:',i["paid"],", fees_balance:",i["fees_balance"])
+    if choice==9:
+         exit()
 
