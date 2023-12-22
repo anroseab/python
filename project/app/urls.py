22797183b8ef4b267ebb5a51c3baf2e5cc3edf82
anroseab <anroseab@gmail.com>
2023-12-22 16:12:19 +0530

    adding

--- project/app/urls.py
+++ project/app/urls.py
@@ -4,4 +4,5 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('',views.index,name='index'),
+    path('about',views.about,name='about')
 ]+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
